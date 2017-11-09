4e7362d01936595e6d568abdadc4c92bf217c1b1
Ross <ross.lo@nogle.com>
2017-11-09 14:48:46 +0800

    feat: test failed case

--- test/max.js
+++ test/max.js
@@ -5,14 +5,14 @@ describe('#max', () => {
   // 測試有沒有取得正確的最大值
   it('should return the maximum in array', done => {
     const maximum = max([1, 10, 100, 1000]);
-    maximum.should.equal(1000);
+    maximum.should.equal(100);
     done()
   })
 
   // 測試有沒有回傳 undefined
   it('should return undefined when array is empty', done => {
     const maximum = max([]);
-    (maximum === undefined).should.be.true;
+    (maximum === undefined).should.be.false;
     done()
   })
 })
